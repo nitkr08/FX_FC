73cd31bec0df707b430ba48c4a2f12675574eecc
sanu <sanu@nitish>
2018-04-26 23:19:24 +0530

    bn

--- PracticeTest/src/test/java/PracticeJava/Array.java
+++ PracticeTest/src/test/java/PracticeJava/Array.java
@@ -4,26 +4,8 @@ public class Array {
 
 	public static void main(String[] args) {
 		
-//		int studentID[]=new int[5];
-//		studentID[0]=90;
-//		studentID[1]=95;
-//		studentID[2]=100;
-//		studentID[3]=105;
-//		studentID[4]=110;
-//		for(int i=0;i<=4;i++){
-//			System.out.println(studentID[i]);
-//		}
+
 		
-		String a="sanu";
-		String b="nitish";
-		if(a==b){
-			
-			System.out.println("both are same");
-		}
-		
-		else{
-			System.out.println("BOth are not same");
-		}
 						
 		
 
