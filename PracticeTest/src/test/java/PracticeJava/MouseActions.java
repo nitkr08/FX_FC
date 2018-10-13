b59196ab7f508b50b7f4116a6005b89ce24cf8fc
sanu <sanu@nitish>
2018-10-13 14:11:03 +0530

    Changes done

--- PracticeTest/src/test/java/PracticeJava/MouseActions.java
+++ PracticeTest/src/test/java/PracticeJava/MouseActions.java
@@ -1,10 +1,10 @@
 package PracticeJava;
 
+import java.awt.AWTException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,22 +20,18 @@ public class MouseActions {
 	public void Start_Browser(){
 		driver=new FirefoxDriver();
 		driver.get("https://www.facebook.com/");
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 	}
-	
+	//Right click operation
 	@Test
-	public void RightLCick_UsingMouse() throws InterruptedException{
+	public void RightLCick_UsingMouse() throws InterruptedException, AWTException{
 		WebElement more_icon=driver.findElement(By.xpath(".//*[@id='login_form']/table/tbody/tr[1]/td[2]/label"));
 		Actions use_mouse=new Actions(driver);
 		//use_mouse.moveToElement(more_icon);
-		use_mouse.doubleClick(more_icon).perform();
-		Thread.sleep(3000);
 		use_mouse.contextClick(more_icon).perform();
-		use_mouse.sendKeys(Keys.ENTER);
 		
 	}
-	
 	
 	//Double click operation using mouse
 	@Test(enabled=false)
