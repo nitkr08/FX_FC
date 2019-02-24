c7c497c26c55d11a37cdf856d5be758a208b6774
sanu <sanu@nitish>
2019-02-24 10:21:54 +0530

    done

---
+++ PracticeTest/src/test/java/PracticeJava/StringEx.java
@@ -0,0 +1,26 @@
+package PracticeJava;
+
+public class StringEx {
+	
+	public void Reverse(String name1){
+		String rev=" ";
+		for(int i=name1.length()-1;i>=0;i--){
+			rev=rev+name1.charAt(i);
+			
+		}
+		System.out.println(rev);
+	}
+	
+	public static void main(String[] args) {
+	
+	String name="My name is nitish";
+	
+	String array[]=name.split(" ");
+	for(String nam:array){
+		StringEx str=new StringEx();
+		str.Reverse(nam);
+		//System.out.println(nam);
+	}	
+	
+	}
+}
